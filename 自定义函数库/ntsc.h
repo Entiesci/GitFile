9c674d12c82c485d95f451d3f28ea8dde493411e
Ntsc-Yrx <hzx223306@163.com>
2021-09-12 18:29:08 +0800

    Update ntsc.h

--- 自定义函数库/ntsc.h
+++ 自定义函数库/ntsc.h
@@ -54,3 +54,72 @@ void change_base(int a, int b) {
 	}
 	cout << to1016[tmp];
 }
+
+//输出
+//参数(数组名,分隔符号,数组开始,数组结尾)
+void out(int s[], char c, int a, int b) {
+	for (int i = a; i < b; i++)
+		cout << s[i] << c;
+	cout << s[b];
+}
+
+//排序库 v2
+//请将要排序的数组设置为全局double数组
+
+//冒泡排序。
+//输入(排序开始项下标,排序结束项下标,列表名)
+void sort_bub(int a, int b, double n[]) {
+	for (int i = a; i <= b; i++) {
+		for (int j = a; j + 1 <= b - i; j++) {
+			if (n[j] > n[j + 1]) {
+				swap(n[j], n[j + 1]);
+			}
+		}
+	}
+}
+
+//选择排序。
+//输入(排序开始项下标,排序结束项下标,列表名)
+void sort_sel(int a, int b, double n[]) {
+	for (int i = a; i <= b ; i++) {
+		int min = i;
+		for (int j = i + 1; j <= b; j++)
+			if (n[j] < n[min])
+				min = j;
+		swap(n[i], n[min]);
+	}
+}
+
+//判断完全数
+//返回bool值
+bool if_perfectnum(int a) {
+	int sum = 0;
+	for (int j = 1; j <= a / 2; j++) {
+		if (a % j == 0)
+			sum += j;
+	}
+	if (sum == a)
+		return true;
+	else
+		return false;
+}
+
+//字符串大小写转换，位移
+//输入(字符串string型,目标字母,目的字母)
+/*实例
+	strlow(s,A,a)  将字符串s中的大写转为小写
+    strlow(s,b,a)  将字符串s中的字母按字母表左移1位
+*/
+//返回字符串string型
+string strlow(string s, char a, char b) {
+	for (int i = 0; i < s.size(); i++) {
+		if (a >= 'a')
+			if (s[i] >= a)
+				s[i] = s[i] - (a - b);
+			else;
+		else if (s[i] < b)
+			s[i] = s[i] - (a - b);
+	}
+	return s;
+}
+
